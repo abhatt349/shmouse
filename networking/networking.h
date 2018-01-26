5a2c063781c5ece8b11837902ffffe6264b9477a
Eric <echen8@stuy.edu>
2018-01-25 19:17:59 -0500

    Added networking files from dwsource

--- networking/networking.h
+++ networking/networking.h
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -5,11 +8,18 @@
 #include <netdb.h>
 #include <string.h>
 #include <errno.h>
+
+#ifndef NETWORKING_H
+#define NETWORKING_H
+
 #define BUFFER_SIZE 256
-#define PORT "11877"
-#define DEFAULT_IP "localhost"
+#define PORT "9001"
+#define TEST_IP "127.0.0.1"
 
 void error_check(int i, char *s);
 int server_setup();
-int client_setup(char * nickname);
-int server_connect;
+int server_connect(int sd);
+int client_setup(char * server);
+
+
+#endif
