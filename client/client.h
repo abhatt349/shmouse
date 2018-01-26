edcf11143378aa988df51751c26adf58b9832606
Gilvir Gill <ggill@stuy.edu>
2018-01-26 05:57:39 -0500

    finished but didnt test clipboard functions

--- client/client.h
+++ client/client.h
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <time.h>
 #include <X11/Xlib.h>
-
+#include <string.h>
 
 Display * open_root_display();
 Window get_root_window(Display * display);
@@ -13,5 +13,8 @@ int get_pointer(Display * display, Window window, int * x, int * y);
 int set_pointer(Display * display, Window window, int dest_x, int dest_y);
 int move_pointer(Display * display, int dest_x, int dest_y);
 
-
+void clipboard_manage(Display * display, Window root, char* (*get)(), void (*set)(char *));
+char * read_clipboard(Display * display, Window root, Atom target); 
+void reject_request(Display * dispay, XSelectionRequestEvent * req);
+void send_utf8(Display * display, XSelectionRequestEvent * req, Atom utf8, char * clipboard);
 
--- client/clipboard.c
+++ client/clipboard.c
@@ -5,7 +5,7 @@
  * "main" method that does all clipboard processing
  * takes function pointers for a get function that returns current clipboard, and a set function that sets that clipboard to be 
  */
-void clipboard_manage(Display * display, Window root, char (*get)(), void (*set)(char *)) {
+void clipboard_manage(Display * display, Window root, char* (*get)(), void (*set)(char *)) {
     Atom sel;
     Atom utf8;
     XEvent event;
@@ -28,7 +28,7 @@ void clipboard_manage(Display * display, Window root, char (*get)(), void (*set)
                 printf("[Clipboard] Selection ownership lost.. resetting clipboard\n");
                 // save the new clipboard in memory and prepare to send it up, then reclaim ownership
                 Atom target = XInternAtom(display, "TEMP", False); // target that we will hold everything in temporarily
-                char * clipboard = read_clipboard(display, root, target)
+                char * clipboard = read_clipboard(display, root, target);
                 (*set)(clipboard);
                 //take back ownership
                 XSetSelectionOwner(display, sel, dummy, CurrentTime);
@@ -40,15 +40,30 @@ void clipboard_manage(Display * display, Window root, char (*get)(), void (*set)
                     reject_request(display, request);
                 }
                 else {
-                    send_utf8(display, request, utf8, get());
+                    send_utf8(display, request, utf8, (*get)());
                 }
                 break;
         }
     }
 }
 
-
+/*
+ * pre: opened display
+ * post: sends no property to request
+ */
 void reject_request(Display * display, XSelectionRequestEvent * req) {
+    XSelectionEvent event;
+    char * property = XGetAtomName(display, req->target);
+    printf("[Clipboard] denying request for property %s\n", property);
+    event.type = SelectionNotify;
+    event.property = None; 
+
+    event.target = req->target;
+    event.time = req->time;
+    event.requestor = req->requestor;
+    event.selection = req->selection;
+
+    XSendEvent(display, req->requestor, True, NoEventMask, (XEvent *) &event);
 }
 /*
  * pre: open display
@@ -70,4 +85,28 @@ void send_utf8(Display * display, XSelectionRequestEvent * req, Atom utf8, char 
 
 }
     
+char * read_clipboard(Display * display, Window root, Atom target) {
+    //when reading the clipboard, it is possible to work with what are called INCRs
+    //int XGetWindowProperty(display, w, property, long_offset, long_length, delete, req_type, 
+    //                        actual_type_return, actual_format_return, nitems_return, bytes_after_return, 
+    //                                                prop_return)
+    Atom actual_type_return;
+    int actual_format_return;
+    unsigned long size, num_items;
+    unsigned char * str;
+    //use this call to get the size, don't actually owrry about saving anything.Free the string afterwards since it saves 0 bits due to the size specification you gave of 0.
+    XGetWindowProperty(display, root, target, 0, 0, False, AnyPropertyType,&actual_type_return, &actual_format_return, &num_items, &size, &str);
+    //X mandates using Xfree() any time you're freeing memory used by x
+    XFree(str);
+    //we don't have time to support incr, so we have to exit if the clipboard is too big.
+    if (actual_type_return == XInternAtom(display, "INCR", False)) {
+        fprintf(stderr, "ERROR: clipboard too big to read in one go\n");
+        exit(0);
+    }
+    Atom actual_actual_type_return;
+    XGetWindowProperty(display, root, target, 0, size, False, AnyPropertyType, &actual_actual_type_return, &actual_format_return, &num_items, &num_items, &str);
+    //we're done reading now, we can now delete the property we were using to get the data off
+    XDeleteProperty(display, root, target);
+    return str;
+}
 
