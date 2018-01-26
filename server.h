f22714a7d61df18020010e16f71159d9e2980666
Gilvir Gill <ggill@stuy.edu>
2018-01-26 18:47:26 -0500

    deleted extra files

--- server.h
+++
@@ -1,7 +0,0 @@
-#include <X11/Xlib.h>
-
-int initialize_server(); //opens shmemory, does initial connectio
-int subserver(int client_socket);
-int close_server();
-int control(int input_pc, int output_pc);
-
