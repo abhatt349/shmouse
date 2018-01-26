62e75c20cbb02651860e2a65de7e4723d146631a
Gillgamesh <ggill@stuy.edu>
2018-01-26 02:30:02 -0500

    x mouse stuff

--- client/client.h
+++ client/client.h
@@ -1,10 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
 #include <X11/Xlib.h>
 
 
-// input functions
+Display * open_root_display();
+Window get_root_window(Display * display);
+Window get_active_window(Display * display);
+void get_resolution(Display * display, int * width, int * height);
+int get_pointer(Display * display, Window window, int * x, int * y);
+int set_pointer(Display * display, Window window, int dest_x, int dest_y);
+int move_pointer(Display * display, int dest_x, int dest_y);
 
-int get_mouse_coords(int *x, int *y);
 
-
-//CLIPBOARD functions
 
--- client/x_manager.c
+++ client/x_manager.c
@@ -14,38 +14,76 @@ Display * open_root_display() {
 }
 
 
-Window * get_active_window() {
+/*
+ * pre: opened display
+ * post: returns root window (encompasses entire screen)
+ */
+Window get_root_window(Display * display) {
+    Window dest_w = DefaultRootWindow(display);
+    //possible error checking for the future
+    return dest_w;
+}
+
+/*
+ * pre: opened display
+ * post: returns currently selected window
+ */
+Window get_active_window(Display * display) {
     Window active;
     int revert_to;
-    XGetInputFocus(display, & focused, &revert_to);
+    XGetInputFocus(display, & active, &revert_to);
     return active;
 }
 
+/*
+ * pre: opened display
+ * post: gets resolution of default screen, important for scaling. most likely cleaner solutions out there but this should do the trick
+ */
+void get_resolution(Display * display, int *width, int * height) {
+    Screen * screen = DefaultScreenOfDisplay(screen);
+    *width = screen-> width;
+    *height = screen->height;
+}
 
-
-
+/*
+ * pre: opened display, most likely window = root window
+ * post: x and y are updated to have the current coords 
+ */
+int get_pointer(Display * display, Window window, int * x, int * y) {
+    *x = 0;
+    *y = 0;
+    int * win_x;
+    int * win_y;
+    unsigned int mask;
+    Window returned;
+    int on_screen = (int) (0 & XQueryPointer(display, window, &returned, &returned, x, y, win_x, win_y, &mask));
+    return on_screen;
+}
 
 
 /*
- *
- *
- *
- *
-*/
-
-
-
-/* hard sets pointer location relative to ROOT window
- *
- *
+ * BEGIN SET METHODS
  */
-int set_pointer(int x, int y) {
-    int src_x = 0;
-    int src_y = 0;
-    
-    XWarpPointer(display, src_w, dest_w, src_x, src_y, src_width, src_height, dest_x,
-                dest_y)
 
+
+/*
+ * pre: opened root window and display
+ * pos:hard sets pointer location relative to ROOT window
+ */
+int set_pointer(Display * display, Window window,int dest_x, int dest_y) {
+    int src_x = 0, src_y = 0;
+    unsigned int src_width=0, src_height=0;    
+    XWarpPointer(display, None, window, src_x, src_y, src_width, src_height, dest_x,
+            dest_y);
+    return 1;
+
+}
+/*
+ * pre: opened root window
+ * post: moves pointer by this delta
+ */
+int move_pointer(Display * display, int dest_x, int dest_y) {
+    return set_pointer(display, None, dest_x, dest_y);
 }
 
 
